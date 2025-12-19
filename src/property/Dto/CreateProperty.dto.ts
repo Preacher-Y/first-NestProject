928391db569b486d70bd4bc46abb6bd88ba0d57a
Preacher-Y <ysheja@gmail.com>
2025-12-19 12:49:43 +0200

    few adjustment on the create property

--- src/property/Dto/CreateProperty.dto.ts
+++ src/property/Dto/CreateProperty.dto.ts
@@ -1,5 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  IsString,
+  IsOptional,
+  IsPositive,
+  IsInt,
+  ValidateNested,
+} from 'class-validator';
+import { CreatePropertyFeatureDto } from 'src/property-feature/dto/create-property-feature.dto';
 
 export class CreatePropertyDto {
   @IsString()
@@ -8,4 +16,24 @@ export class CreatePropertyDto {
     example: 'Ocean View Apartment',
   })
   name: string;
+
+  @IsOptional()
+  @IsInt()
+  @IsPositive()
+  @ApiProperty({
+    description: 'Owner user id (ownerID)',
+    example: 1,
+    required: false,
+  })
+  ownerId?: number;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => CreatePropertyFeatureDto)
+  @ApiProperty({
+    description: 'Optional property feature payload',
+    type: () => CreatePropertyFeatureDto,
+    required: false,
+  })
+  propertyFeature?: CreatePropertyFeatureDto;
 }
